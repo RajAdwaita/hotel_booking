16387b0b6f18f23e9832a220821eaf772ebdc380
Adwaita Raj Modak <adwaitaraj@gmail.com>
2023-06-07 02:01:30 +0530

    Security Issues

---
+++ travel/app/components/navbar/Categories.tsx
@@ -0,0 +1,20 @@
+import Container from "../Container";
+
+const Categories = () => {
+    return (
+        <Container>
+            <div className="
+            pt-4
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            overflow-x-auto
+            ">
+
+            </div>
+        </Container>
+    );
+}
+
+export default Categories;
