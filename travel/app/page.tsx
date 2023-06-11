988b99d514dc4282c049fe37d1565a3eb5490748
Adwaita Raj Modak <adwaitaraj@gmail.com>
2023-06-11 17:01:56 +0530

    setting up Booking

--- travel/app/page.tsx
+++ travel/app/page.tsx
@@ -4,8 +4,10 @@ import Image from 'next/image'
 import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState';
-import getListings from './actions/getListings';
-import ListingCard from './components/listing/ListingCard';
+import getListings, {
+  IListingsParams
+} from "@/app/actions/getListings";
+import ListingCard from './components/listings/ListingCard';
 import getCurrentUser from './actions/getCurrentUser';
 
 export default async function Home() {
